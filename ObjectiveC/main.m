9c197487c5d02d74188376025635e5e81cef50ac
Zay Johnson <zayjohnson@Zays-MacBook-Pro.local>
2012-09-17 13:27:14 -0400

    Initial Commit

---
+++ ObjectiveC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjectiveC
+//
+//  Created by Zay Johnson on 9/17/12.
+//  Copyright (c) 2012 Zay Johnson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
